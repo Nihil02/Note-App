204810634bb81315e287c88236d6bc7c5fba90a6
Nihil02 <samuelalva02@gmail.com>
2023-03-23 14:23:56 -0600

    client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import NoteList from "./component/NoteList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <p>Yes</p>
-  )
+    <>
+      <NoteList />
+    </>
+  );
 }
 
-export default App
+export default App;
--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 const pool = require("./db");
+
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use(express.json());
 
 //Routing
